f810a0144c4df08c4320c12445dd7511ebb99f87
Anna Burova <burova.anna83@mail.ru>
2024-05-29 18:25:13 +0300

    new

--- src/test/java/ru/netology/rest/mobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/mobileBankApiTestV3.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-class MobileBankApiTestV3 {
+class mobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
